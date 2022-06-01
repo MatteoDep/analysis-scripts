9575b01abd0bd72576c1b6271844efe9a1572c5b
MatteoDep <matteo.dep97@gmail.com>
2022-06-02 01:27:34 +0200

    using high bias and fixed plots completely

--- temp_characterization.py
+++ temp_characterization.py
@@ -17,14 +17,14 @@ import analysis as a
 EXPERIMENT = 'temp_characterization'
 
 
-def temp_dependence(names, prefix=""):
+def temp_dependence(names, lowtemp_lowbias_name=None, lowtemp_highbias_name=None):
     """Main analysis routine.
     names: list
     """
     global dh
     fields = np.concatenate([[0.02], np.arange(0.05, 2, 0.05)]) * ur['V/um']
     delta_field = 0.02 * ur['V/um']
-    temp_field_thresh = 1.1 * ur['V*K/um']
+    temp_field_thresh = 1 * ur['V*K/um']
 
     temperature = [] * ur.K
     conductance = []
@@ -34,15 +34,16 @@ def temp_dependence(names, prefix=""):
         temperature0 = dh.get_temperature()
         temperature = np.append(temperature, temperature0)
         if j == 0:
-            length0 = dh.get_length()
-            print("Pair {} of length {}.".format(dh.prop['pair'], length0.to('um')))
-            cond = dh.data['voltage'] > temp_field_thresh * length0 / temperature0
-            conductance_lowtemp = (dh.data['current'][cond] / dh.data['voltage'][cond]).to('S')
-            fields_lowtemp = dh.data['voltage'][cond] / length0
+            length = dh.get_length()
+            chip_name = dh.chip_name
+            pair = dh.prop['pair']
+            print("Pair {} of length {}.".format(pair, length.to('um')))
+            if lowtemp_lowbias_name is None:
+                lowtemp_lowbias_name = name
         for i, field in enumerate(fields):
             bias_win = [] * ur.V
-            bias_win = np.append(bias_win, (field - delta_field) * length0)
-            bias_win = np.append(bias_win, (field + delta_field) * length0)
+            bias_win = np.append(bias_win, (field - delta_field) * length)
+            bias_win = np.append(bias_win, (field + delta_field) * length)
             if i < max_i and max(field, delta_field) * temperature0 > temp_field_thresh:
                 conductance0 = dh.get_conductance(bias_win=bias_win, time_win=[0.25, 0.75])
                 if conductance0 is None:
@@ -63,7 +64,7 @@ def temp_dependence(names, prefix=""):
         conductance[i] = conductance[i][indices]
 
     # plot temperature dependence
-    temp_win = [101, 350] * ur.K
+    temp_win = [100, 350] * ur.K
     cond = a.is_between(temperature, temp_win)
     x = 100 / temperature
     x_, dx, ux = a.separate_measurement(x)
@@ -82,17 +83,18 @@ def temp_dependence(names, prefix=""):
             x1 = 100 / temp_win.magnitude
             ax.plot(x1, model(x1), c='r', zorder=len(fields), label=fr"$U_A = {act_energy}$")
     ax.legend()
-    ax.set_title("Conductance")
+    ax.set_title(f"Conductance ({chip_name} {pair})")
     ax.set_xlabel(fr"$\frac{{100}}{{T}}$ [${ux:~L}$]")
     ax.set_ylabel(fr"$G$ [${uy:~L}$]")
     ax.set_yscale('log')
-    res_image = os.path.join(res_dir, f"{prefix}temperature_dep.png")
+    res_image = os.path.join(res_dir, f"{chip_name}_{pair}_temperature_dep.png")
     fig.savefig(res_image, dpi=300)
     plt.close()
 
     # Power law
+    # temperature dependence at low bias
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 9))
-    temp_win = [20, 100] * ur.K
+    temp_win = [max(20 * ur.K, temp_field_thresh / fields[0]), 100 * ur.K]
     cond = a.is_between(temperature, temp_win)
     x = temperature[cond]
     y = conductance[0][cond]
@@ -102,29 +104,36 @@ def temp_dependence(names, prefix=""):
     x_, dx, ux = a.separate_measurement(x)
     y_, dy, uy = a.separate_measurement(y)
     ax1.errorbar(x_, y_, xerr=dx, yerr=dy, fmt='o', zorder=1, label='data')
-    ax1.plot(x_, model(x_), 'r', zorder=2, label='fit')
-    ax1.set_title(fr'$\alpha = {alpha}$')
+    ax1.plot(x_, model(x_), zorder=2, label=fr'$\alpha = {alpha}$')
+    ax2.set_title(f'Temperature Power Law ({chip_name} {pair})')
     ax1.set_xscale('log')
     ax1.set_yscale('log')
     ax1.set_xlabel(fr"$T$ [${ux:~L}$]")
     ax1.set_ylabel(fr"$G$ [${uy:~L}$]")
     ax1.legend()
-    x = fields_lowtemp
-    y = conductance_lowtemp
-    coeffs, model = a.fit_powerlaw(x, y)
-    beta = coeffs[0]
-    print("beta:", beta)
-    x_, dx, ux = a.separate_measurement(x)
-    y_, dy, uy = a.separate_measurement(y)
-    ax2.errorbar(x_, y_, xerr=dx, yerr=dy, fmt='o', zorder=1, label='data')
-    ax2.plot(x_, model(x_), c='r', zorder=2, label='fit')
-    ax2.set_title(fr'$\beta = {beta}$')
+    # bias dependence at low temperature
+    temp_field_thresh_ = temp_field_thresh
+    for i, name in enumerate([lowtemp_lowbias_name, lowtemp_highbias_name]):
+        dh.load(name)
+        temperature = dh.get_temperature()
+        cond = dh.data['voltage'] > temp_field_thresh_ * length / temperature
+        x = dh.data['voltage'][cond] / length
+        y = (dh.data['current'][cond] / dh.data['voltage'][cond]).to('S')
+        coeffs, model = a.fit_powerlaw(x, y)
+        beta = coeffs[0]
+        print(f"beta_{i+1}:", beta)
+        x_, dx, ux = a.separate_measurement(x)
+        y_, dy, uy = a.separate_measurement(y)
+        ax2.errorbar(x_, y_, xerr=dx, yerr=dy, fmt='o', zorder=1)
+        ax2.plot(x_, model(x_), zorder=2, label=fr'$\beta = {beta}$')
+        temp_field_thresh_ = dh.prop['voltage'] * temperature / length
+    ax2.set_title(f'Bias Power Law ({chip_name} {pair})')
     ax2.set_xscale('log')
     ax2.set_yscale('log')
     ax2.set_xlabel(fr"$V_b$ [${ux:~L}$]")
     ax2.set_ylabel(fr"$G$ [${uy:~L}$]")
     ax2.legend()
-    res_image = os.path.join(res_dir, f"{prefix}power_law.png")
+    res_image = os.path.join(res_dir, f"{chip_name}_{pair}_power_law.png")
     fig.savefig(res_image, dpi=300)
     plt.close()
 
@@ -159,14 +168,14 @@ if __name__ == "__main__":
     # pair = "P15-P17"
     # pair = "P7-P8"
     pair = "P13-P14"
-    prefix = f"{chip}_{pair}_"
     data_dir = os.path.join('data', chip)
     res_dir = os.path.join('results', EXPERIMENT, chip)
     os.makedirs(res_dir, exist_ok=True)
 
     dh = a.DataHandler(data_dir)
 
-    biases = np.arange(0, 14, 2)
+    lowtemp_lowbias_name = None
+    lowtemp_highbias_num = None
     if chip == 'SPC2':
         if pair == 'P2-P4':
             nums = np.arange(45, 74)
@@ -182,9 +191,10 @@ if __name__ == "__main__":
                                    np.arange(202, 203),
                                    np.arange(204, 208),
                                    np.arange(209, 226)])
-            biases = np.arange(0, 5)
+            lowtemp_lowbias_name = f"{chip}_195"
+            lowtemp_highbias_name = f"{chip}_277"
     if chip == 'SOC3':
         nums = np.arange(49, 91)
     names = [f"{chip}_{i}" for i in nums]
 
-    temp_dependence(names, prefix=prefix)
+    temp_dependence(names, lowtemp_lowbias_name=lowtemp_lowbias_name, lowtemp_highbias_name=lowtemp_highbias_name)
