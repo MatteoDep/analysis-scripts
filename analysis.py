reformat and correcting offset in universal scaling

analysis.py
@@ -47,26 +47,37 @@ class DataHandler:
     """
     Loads data and builds quantities arrays.
     """
-    DEFAULT_OFFSET_MASK_ARGS = {'field_win': [-0.05, 0.05]*ur['V/um'], 'time_win': [0.25, 0.75]}
+    PARAMS = {
+        'only_return': True,
+        'correct_offset': True,
+        'offset_mask_args': {'field_win': [-0.05, 0.05]*ur['V/um'], 'only_return': True},
+    }
 
-    def __init__(self, data_dir='data', chips_dir='chips'):
+    def __init__(self, data_dir='data', chips_dir='chips', **params):
         self.data_dir = data_dir
         self.chips_dir = chips_dir
-
+        self.PARAMS.update(params)
         self.props = {}
-        self.cps = {}
+        self.cp_cache = {}
+        self.cp = None
         self.chip = None
+        self.clear()
+        pass
+
+    def clear(self):
         self.name = None
-        self.raws = {}
+        self.cache = {}
         self.raw = None
-        self.length = None
+        self.prop = None
         pass
 
     def load_chip(self, chip, names=None):
-        """Load properties file."""
+        """Load properties file.
+        Note: assumes that names are not shared between different chips."""
         # extend chip parameters dict
         cp_path = os.path.join(self.chips_dir, chip + '.json')
-        self.cps[chip] = ChipParameters(cp_path)
+        self.cp_cache[chip] = ChipParameters(cp_path)
+        self.cp = self.cp_cache[chip]
         self.chip = chip
         # extend properties dict
         props_path = os.path.join(self.data_dir, chip, 'properties.csv')
@@ -93,81 +104,80 @@ class DataHandler:
 
     def load(self, name):
         """Read data from files."""
-        old_name = self.name
-        if name == old_name:
-            return self.raw
-
-        # load data
-        self.name = name
-        self.chip = self.name.split('_')[0]
-        self.prop = self.props[name]
-        if name in self.raws:
-            self.raw = self.raws[name]
+        if not isinstance(name, str):
+            names = name
+            for i, name in enumerate(names):
+                print(f"\rLoading {i+1} of {len(names)}.", end='', flush=True)
+                self.load(name)
+            print()
         else:
-            self.raw = {}
-            path_name = os.path.join(self.data_dir, self.chip, name)
-            if os.path.isfile(path_name + '.csv'):
-                df = pd.read_csv(path_name + '.csv')
-                old_keys = ['x1', 'y', 'x2', 'temp', 't']
-                keys = [o.replace('i', 'current').replace('v', 'bias') for o in self.prop['order']] \
-                    + ['gate', 'temperature', 'time']
-                for old_key, key in zip(old_keys, keys):
-                    self.raw[key] = ur.Quantity(df[old_key].to_numpy(), DEFAULT_UNITS[key])
-            else:
-                raise FileNotFoundError(f"Could not load name '{name}'. File does not exist.")
-            self.raws[name] = self.raw
-
-        # reset quantities
-        if old_name is None or self.props[old_name]['pair'] != self.prop['pair']:
-            self.length = None
+            self.name = name
+            self.prop = self.props[name]
+            if name not in self.cache:
+                self.cache[name] = {}
+                path_name = os.path.join(self.data_dir, self.chip, name)
+                if os.path.isfile(path_name + '.csv'):
+                    df = pd.read_csv(path_name + '.csv')
+                    old_keys = ['x1', 'y', 'x2', 'temp', 't']
+                    keys = [o.replace('i', 'current').replace('v', 'bias') for o in self.prop['order']] \
+                        + ['gate', 'temperature', 'time']
+                    for old_key, key in zip(old_keys, keys):
+                        self.cache[name][key] = ur.Quantity(df[old_key].to_numpy(), DEFAULT_UNITS[key])
+                else:
+                    raise FileNotFoundError(f"Could not load name '{name}'. File does not exist.")
+            self.raw = self.cache[name]
         return self.raw
 
-    def get_mask(self, field_win=None, time_win=None, bias_win=None, current_win=None):
+    def set_default_params(self, **params):
+        self.PARAMS.update(params)
+
+    def get_mask(self, field_win=None, time_win=None, bias_win=None, current_win=None, **params):
         """Generate mask to be applied to data.
         Note: field_win will compute bias_win = length * field_win.
         """
-        mask = np.ones(self.raw['bias'].shape, dtype=bool)
+        params = self._get_tmp_params(**params)
+        mask = np.ones(self.raw['time'].shape, dtype=bool)
         if time_win is not None:
             mask *= is_between(self.raw['time'], time_win)
         if field_win is not None:
             if bias_win is not None:
                 warnings.warn("argument 'field_win' ignored because 'bias_win' takes precedence.", UserWarning)
             else:
-                if isinstance(field_win, list):
-                    field_win = qlist2qarray(field_win)
-                bias_win = field_win * self.get_length()
+                mask *= is_between(self.get_field(), field_win)
         if bias_win is not None:
             mask *= is_between(self.raw['bias'], bias_win)
         if current_win is not None:
-            mask *= is_between(self.raw['current'], current_win)
+            mask *= is_between(self.get_current(**params), current_win)
+        if params['only_return']:
+            direction = np.diff(self.get_bias().m) > 0
+            direction = np.append(direction, direction[0])
+            mask *= direction != direction[0]
         return mask
 
-    def get_current(self, correct_offset=False, noise_level=None, **offset_mask_args):
+    def get_field(self, **kwargs):
+        return self.get_bias(**kwargs) / self.get_length()
+
+    def get_current(self, mask=None, **params):
         current = self.raw['current'].copy()
-        if correct_offset:
-            if len(offset_mask_args) == 0:
-                offset_mask_args = self.DEFAULT_OFFSET_MASK_ARGS
-            offset_mask = self.get_mask(**offset_mask_args)
-            current -= np.mean(current[offset_mask])
-        if noise_level is not None:
-            current[np.abs(current) < noise_level] = np.nan
+        params = self._get_tmp_params(**params)
+        if self.prop['injection'] == '2P':
+            if params['correct_offset']:
+                offset_mask = self.get_mask(**params['offset_mask_args'])
+                current -= np.mean(current[offset_mask])
+        if mask is not None:
+            current = self.apply_mask(current, mask)
         return current
 
-    def get_conductance(self, method='all', **kwargs):
+    def get_conductance(self, method='all', mask=None, **params):
         """Calculate conductance."""
-        wins = {k: v for k, v in kwargs.items() if k in self.get_mask.__code__.co_varnames}
-        curr_kwargs = {k: v for k, v in kwargs.items() if k in self.get_current.__code__.co_varnames}
-        bias = self.raw['bias'].copy()
-        current = self.get_current(**curr_kwargs)
-        mask = self.get_mask(**wins)
-        mask *= bias != 0*ur.V
-        notmask = np.where(mask, False, True)
-        current[notmask] = np.nan
-        bias[notmask] = np.nan
+        bias = self.get_bias()
+        bias = self.apply_mask(bias, bias != 0*ur.V)
         if method == 'fit':
+            current = self.get_current(mask=mask, correct_offset=False)
             coeffs, _ = fit_linear(bias, current)
             conductance = coeffs[0]
         else:
+            current = self.get_current(mask=mask, **params)
             conductance = current / bias
             if method == 'average':
                 conductance = average(conductance)
@@ -178,19 +188,46 @@ class DataHandler:
     def get_resistance(self, **kwargs):
         return (1 / self.get_conductance(**kwargs)).to(DEFAULT_UNITS['resistance'])
 
-    def get_temperature(self):
-        return average(self.raw['temperature'])
+    def get_temperature(self, **kwargs):
+        return self._get_simple('temperature', **kwargs)
 
-    def get_gate(self):
-        return average(self.raw['gate'])
+    def get_bias(self, **kwargs):
+        return self._get_simple('bias', **kwargs)
+
+    def get_time(self, **kwargs):
+        return self._get_simple('time', **kwargs)
+
+    def get_gate(self, **kwargs):
+        return self._get_simple('gate', **kwargs)
+
+    @staticmethod
+    def apply_mask(q, mask):
+        qres = q.copy()
+        notmask = np.where(mask, False, True)
+        qres[notmask] = np.nan
+        return qres
+
+    def _get_simple(self, key, method='all', mask=None):
+        q = self.raw[key]
+        if mask is not None:
+            q = self.apply_mask(q)
+        if method == 'average':
+            q = average(q)
+        elif method != 'all':
+            raise ValueError(f'Unknown method {method}.')
+        return q.to(DEFAULT_UNITS[key])
+
+    def _get_tmp_params(self, **params):
+        for key in self.PARAMS:
+            if key not in params:
+                params[key] = self.PARAMS[key]
+        return params
 
     def get_length(self):
-        if self.length is None:
-            self.length = self.cps[self.chip].get_distance(self.prop['pair'])
-        return self.length
+        return self.cp.get_distance(self.prop['pair'])
 
-    def plot(self, ax, mode='i/v', correct_offset=False, label=None,
-             color=None, set_xy_label=True, **wins):
+    def plot(self, ax, mode='i/v', mask=None, label=None,
+             color=None, set_xy_label=True):
         key_short_list = ['t', 'v', 'vg', 'i']
         key_list = ['time', 'bias', 'gate', 'current']
         sym_list = ['t', 'V', 'V_G', 'I']
@@ -198,17 +235,17 @@ class DataHandler:
         xkey = key_list[key_short_list.index(xkey)]
         ykeys = [key_list[key_short_list.index(ykey)] for ykey in ykeys.split('-')]
         x = self.raw[xkey]
+        if mask is not None:
+            x = x[mask]
         if label is not None:
             label = label.replace('{', '{0.').format(self)
 
         for ykey in ykeys:
             y = self.raw[ykey]
+            if mask is not None:
+                y = y[mask]
             # correct data
-            if correct_offset and ykey == 'current':
-                offset_mask = self.get_mask(**self.offset_mask_args)
-                y -= np.mean(y[offset_mask])
-            mask = self.get_mask(**wins)
-            ax.plot(x[mask].m, y[mask].m, '.', label=label, c=color)
+            ax.plot(x.m, y.m, '.', label=label, c=color)
             if set_xy_label:
                 ysym = sym_list[key_list.index(ykey)]
                 ax.set_ylabel(f"${ysym}$" + ulbl(y.u))
@@ -239,6 +276,7 @@ class ChipParameters():
     def __init__(self, config_path=None):
         if config_path is not None:
             self.load(config_path)
+        self.cache = {}
 
     def load(self, config_path):
         """
@@ -259,8 +297,10 @@ class ChipParameters():
 
     def get_distance(self, pair):
         """Get distance between 2 cojtacts."""
-        k1, k2 = pair.split('-')
-        return np.abs(self.positions[k2] - self.positions[k1])
+        if pair not in self.cache:
+            k1, k2 = pair.split('-')
+            self.cache[pair] = np.abs(self.positions[k2] - self.positions[k1])
+        return self.cache[pair]
 
 
 # MEASUREMENTS HELP FUNCTIONS
@@ -288,12 +328,12 @@ def strip_err(x):
     return x_ * ux
 
 
-def qlist2qarray(qlist):
+def qlist_to_qarray(qlist):
     """Change a list of quantities to a quantity array."""
     ux = None
     mlist = []
     for x in qlist:
-        if x is None:
+        if x is None or isnan(x):
             x = np.nan
         if hasattr(x, 'units'):
             if ux is not None:
@@ -301,9 +341,23 @@ def qlist2qarray(qlist):
             ux = x.u
             mlist.append(x.m)
         else:
-            mlist.append(x)
+            raise ValueError('Quantity list contains non quantity items.')
     assert ux is not None, 'No element of the list was a quantity!'
     return mlist * ux
+
+
+def q_from_df(df, key, units=ur['']):
+    x = []
+    for i, index in enumerate(df.index):
+        x.append(df.loc[index, key] * units)
+        if f'd_{key}' in df:
+            dx = df.loc[index, f'd_{key}']
+            x[i] = x[i].plus_minus(dx)
+    if len(x) == 1:
+        x = x[0]
+    else:
+        x = np.concatenate(x)
+    return x
 
 
 def isnan(x):

data_plotter.py
@@ -15,48 +15,28 @@ import analysis as a
 from analysis import ur
 
 
-def main():
-    global names, modes, labels, res_names, titles, x_win, y_win
-    global colors, show, save, correct_offset
-
-    for i, names_ in enumerate(names):
-        n_sp = len(modes)
-        fig, axs = plt.subplots(n_sp, 1, figsize=(7, n_sp*4))
-        if n_sp == 1:
-            axs = [axs]
-        for name, label, color in zip(names_, labels[i], colors[i]):
-            dh.load(name)
-            for j, m in enumerate(modes):
-                axs[j] = dh.plot(axs[j], mode=m, label=label, color=color,
-                                 x_win=x_win, y_win=y_win)
-        if titles is not None:
-            fig.suptitle(titles[i])
-        if not np.array([label is None for label in labels[i]]).all():
-            plt.legend()
-        if show:
-            plt.show()
-        if save:
-            fig.savefig(res_paths[i], dpi=100)
-
-
-# OTHER FUNCIONS
-
-def plot_iv_with_inset(dh, res_dir):
-    field_win = [-0.05, 0.05] * ur['V/um']
-    fig, ax = plt.subplots(figsize=(12, 9))
-    dh.plot(ax)
-    if dh.prop['temperature'] < 55*ur.K:
-        ax_in = ax.inset_axes([0.65, 0.08, 0.3, 0.3])
-        dh.plot(ax_in, field_win=field_win, set_xy_label=False)
-        ax.indicate_inset_zoom(ax_in)
-    fig.suptitle(f"{dh.chip} {dh.prop['pair']} at {dh.prop['temperature']}")
-    res_image = os.path.join(
-        res_dir, f"{dh.chip}_{dh.prop['pair']}_iv_{a.fmt(dh.prop['temperature'], fmt='')}.png"
-    )
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', category=UserWarning)
-        fig.savefig(res_image, dpi=100)
-    plt.close()
+def plot_iv_with_inset(dh, names, res_dir):
+    for i, name in enumerate(names):
+        print(f"\rPlotting iv {i+1} of {len(names)}.", end='', flush=True)
+        dh.load(name)
+        fig, ax = plt.subplots(figsize=(12, 9))
+        dh.plot(ax)
+        if dh.prop['temperature'] < 55*ur.K:
+            mask = dh.get_mask([-0.05, 0.05] * ur['V/um'])
+            ax_in = ax.inset_axes([0.65, 0.08, 0.3, 0.3])
+            dh.plot(ax_in, mask=mask, set_xy_label=False)
+            ax_in.set_xmargin(0)
+            ax_in.set_ymargin(0)
+            ax.indicate_inset_zoom(ax_in)
+        fig.suptitle(f"{dh.chip} {dh.prop['pair']} at {dh.prop['temperature']}")
+        res_image = os.path.join(
+            res_dir, f"{dh.chip}_{dh.prop['pair']}_iv_{a.fmt(dh.prop['temperature'], sep='')}.png"
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', category=UserWarning)
+            fig.savefig(res_image, dpi=100)
+        plt.close()
+    print()
 
 
 def get_cbar_and_cols(fig, values, log=False, ticks=None, **kwargs):
@@ -136,4 +116,21 @@ if __name__ == "__main__":
     if not isinstance(modes, list):
         modes = [modes]
 
-    main()
+    for i, names_ in enumerate(names):
+        n_sp = len(modes)
+        fig, axs = plt.subplots(n_sp, 1, figsize=(7, n_sp*4))
+        if n_sp == 1:
+            axs = [axs]
+        for name, label, color in zip(names_, labels[i], colors[i]):
+            dh.load(name)
+            for j, m in enumerate(modes):
+                axs[j] = dh.plot(axs[j], mode=m, label=label, color=color,
+                                 x_win=x_win, y_win=y_win)
+        if titles is not None:
+            fig.suptitle(titles[i])
+        if not np.array([label is None for label in labels[i]]).all():
+            plt.legend()
+        if show:
+            plt.show()
+        if save:
+            fig.savefig(res_paths[i], dpi=100)

gate.py
@@ -17,30 +17,31 @@ from data_plotter import get_cbar_and_cols
 EXPERIMENT = os.path.splitext(os.path.basename(__file__))[0]
 
 
-def get_gate_dependence(dh, names, fields, delta_field, noise_level=0.5*ur.pA):
+def get_gate_dependence(dh, names, fields, delta_field):
     gate = [] * ur.V
     conductance = [[] * ur.S for f in fields]
     for i, name in enumerate(names):
         dh.load(name)
-        gate_ = dh.get_gate()
+        gate_ = dh.get_gate(method='average')
         gate = np.append(gate, gate_)
-        full_conductance = dh.get_conductance(correct_offset=True, time_win=[0.25, 0.75])
-        full_conductance_denoised = dh.get_conductance(correct_offset=True, noise_level=noise_level, time_win=[0.25, 0.75])
+        mask_denoise = dh.get_mask(current_win=[0.5*ur.pA, 1])
+        full_conductance = dh.get_conductance()
+        full_conductance_denoised = dh.get_conductance(mask=mask_denoise)
         for j, field in enumerate(fields):
             field_win = [field - delta_field, field + delta_field]
             if dh.prop['bias'] < field_win[1] * dh.get_length():
-                conductance_ = np.nan
+                conductance_i = np.nan
             else:
-                mask = dh.get_mask(field_win=field_win, time_win=[0.25, 0.75])
+                mask = dh.get_mask(field_win=field_win)
                 if np.sum(a.isnan(full_conductance_denoised[mask])) < np.sum(mask):
-                    conductance_ = a.average(full_conductance[mask])
+                    conductance_i = a.average(full_conductance[mask])
                 else:
-                    conductance_ = np.nan
-            conductance[j] = np.append(conductance[j], conductance_)
+                    conductance_i = np.nan
+            conductance[j] = np.append(conductance[j], conductance_i)
     return gate, conductance
 
 
-def main(data_dict, noise_level=0.5*ur.pA):
+def main(data_dict):
     """Main analysis routine.
     names: list
     """
@@ -55,18 +56,8 @@ def main(data_dict, noise_level=0.5*ur.pA):
             nums = data_dict[chip][pair]['nums']
             names = np.array([f"{chip}_{i}" for i in nums])
 
-            dh.load(names[0])
-            print("Chip {}, Pair {} of length {}.".format(chip, pair, fmt(dh.get_length())))
-
-            if 'noise_level' in data_dict[chip][pair]:
-                nl = data_dict[chip][pair]['noise_level']
-            else:
-                nl = noise_level
-
-            for i, name in enumerate(names):
-                print(f"\rLoading {i+1} of {len(names)}.", end='', flush=True)
-                dh.load(name)
-            print()
+            dh.load(names)
+            print(f"Chip {chip}, Pair {pair} of length {fmt(dh.get_length())}.")
 
             names_dict = {}
             for name in names:
@@ -79,13 +70,13 @@ def main(data_dict, noise_level=0.5*ur.pA):
                 print(f'Processing temperature {temp_key}')
                 names = names_dict[temp_key]
                 dh.load(names[0])
-                max_field = np.amin(a.qlist2qarray([dh.props[name]['bias'] for name in names])) / dh.get_length()
+                max_field = np.amin(a.qlist_to_qarray([dh.props[name]['bias'] for name in names])) / dh.get_length()
                 fact = max(a.separate_measurement((fields[4] + delta_field) // max_field)[0] + 1, 1)
                 fields_ = fields / fact
                 delta_field_ = delta_field / fact
                 fields_ = fields_[fields_ + delta_field_ < max_field]
 
-                gate, conductance = get_gate_dependence(dh, names, fields_, delta_field_, noise_level=nl)
+                gate, conductance = get_gate_dependence(dh, names, fields_, delta_field_)
 
                 ranges = []
                 directions = []
@@ -110,7 +101,8 @@ def main(data_dict, noise_level=0.5*ur.pA):
                     y = conductance[i]
                     y_, dy, uy = a.separate_measurement(y)
                     for d, (j0, j1) in zip(directions, ranges):
-                        ax.errorbar(x_[j0:j1], y_[j0:j1], xerr=dx[j0:j1] if dx is not None else None,
+                        ax.errorbar(x_[j0:j1], y_[j0:j1],
+                                    xerr=dx[j0:j1] if dx is not None else None,
                                     yerr=dy[j0:j1] if dx is not None else None,
                                     ls=ls[d], marker=ms[d], zorder=i, c=cols[i])
                 ax.legend(handles=[
@@ -122,7 +114,7 @@ def main(data_dict, noise_level=0.5*ur.pA):
                 ax.set_yscale('log')
                 cbar.ax.set_ylabel("$E_{{bias}}$" + ulbl(fields.u))
                 res_image = os.path.join(res_dir, f"{chip}_{pair}_{temp_key}_gate_dep.png")
-                fig.savefig(res_image, dpi=100)
+                fig.savefig(res_image)
                 plt.close()
 
 
@@ -177,4 +169,4 @@ if __name__ == "__main__":
         },
     }
 
-    main(data_dict, noise_level=0.5 * ur.pA)
+    main(data_dict)

length.py
@@ -18,7 +18,6 @@ EXPERIMENT = os.path.splitext(os.path.basename(__file__))[0]
 
 def get_length_dependence(names, field, delta_field):
     global dh, res_dir, plot_ivs
-    time_win = [0.25, 0.75]
     method = 'fit' if field == 0*ur['V/um'] else 'average'
 
     injs = ['2p', '4p']
@@ -30,13 +29,13 @@ def get_length_dependence(names, field, delta_field):
         inj = dh.prop['injection'].lower()
         length[inj] = np.append(length[inj], dh.get_length())
         pair[inj] = np.append(pair[inj], dh.prop['pair'])
-        field_win = [field - delta_field, field + delta_field]
-        resistance0 = dh.get_resistance(method=method, time_win=time_win, field_win=field_win)
+        mask = dh.get_mask([field - delta_field, field + delta_field])
+        resistance0 = dh.get_resistance(method=method, mask=mask)
         resistance[inj] = np.append(resistance[inj], resistance0)
     return length, resistance, pair
 
 
-def main(data_dict, noise_level=0.5*ur.pA, pair_on_axis=False):
+def main(data_dict, pair_on_axis=False):
     """Compute resistance over length characterization."""
     global dh, res_dir
 
@@ -49,11 +48,8 @@ def main(data_dict, noise_level=0.5*ur.pA, pair_on_axis=False):
         nums = data_dict[chip]['nums']
         names = np.array([f"{chip}_{i}" for i in nums])
 
+        dh.load(names)
         print("Chip {}.".format(chip))
-        for i, name in enumerate(names):
-            print(f"\rLoading {i+1} of {len(names)}.", end='', flush=True)
-            dh.load(name)
-        print()
 
         names_dict = {}
         for name in names:
@@ -139,7 +135,7 @@ def main(data_dict, noise_level=0.5*ur.pA, pair_on_axis=False):
                     ax.set_xlabel("Length" + ulbl(ux))
                 ax.set_ylabel("$R_{cont}$" + ulbl(uy))
                 res_image = os.path.join(res_dir, f"{temp_key}_contact_resistance.png")
-                plt.savefig(res_image, dpi=100)
+                plt.savefig(res_image)
 
 
 if __name__ == "__main__":

temperature.py
@@ -20,63 +20,57 @@ from data_plotter import plot_iv_with_inset, get_cbar_and_cols
 EXPERIMENT = os.path.splitext(os.path.basename(__file__))[0]
 
 
-def get_temperature_dependence(dh, names, fields, delta_field, noise_level=0.5*ur.pA):
+def get_temperature_dependence(dh, names, fields, delta_field):
     temperature = [] * ur.K
     conductance = [[] * ur.S for f in fields]
     max_j = len(fields)
     for i, name in enumerate(names):
         dh.load(name)
-        temperature_ = dh.get_temperature()
+        temperature_ = dh.get_temperature(method='average')
         temperature = np.append(temperature, temperature_)
-        full_conductance = dh.get_conductance(correct_offset=True, time_win=[0.25, 0.75])
-        full_conductance_denoised = dh.get_conductance(correct_offset=True, time_win=[0.25, 0.75], noise_level=noise_level)
+        mask_denoise = dh.get_mask(current_win=[0.5*ur.pA, 1])
+        full_conductance = dh.get_conductance()
+        full_conductance_denoised = dh.get_conductance(mask=mask_denoise)
         for j, field in enumerate(fields):
             if j < max_j:
                 field_win = [field - delta_field, field + delta_field]
                 if dh.prop['bias'] < field_win[1] * dh.get_length():
                     max_j = j
-                    conductance_ = np.nan
+                    conductance_i = np.nan
                 else:
-                    mask = dh.get_mask(field_win=field_win, time_win=[0.25, 0.75])
+                    mask = dh.get_mask(field_win=field_win)
                     if np.sum(a.isnan(full_conductance_denoised[mask])) < np.sum(mask):
-                        conductance_ = a.average(full_conductance[mask])
+                        conductance_i = a.average(full_conductance[mask])
                     else:
-                        conductance_ = np.nan
+                        conductance_i = np.nan
             else:
-                conductance_ = np.nan
-            conductance[j] = np.append(conductance[j], conductance_)
+                conductance_i = np.nan
+            conductance[j] = np.append(conductance[j], conductance_i)
     return temperature, conductance
 
 
-def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv=False):
+def main(data_dict, adjust_alpha=False, plot_iv=False):
     """Main analysis routine.
     names: list
     """
     fields = np.concatenate([[0.02], np.arange(0.05, 2, 0.05)]) * ur['V/um']
     delta_field = 0.01 * ur['V/um']
     dh = a.DataHandler()
+
     if adjust_alpha:
         csv_path = os.path.join('results', EXPERIMENT, 'params_adjusted.csv')
     else:
         csv_path = os.path.join('results', EXPERIMENT, 'params.csv')
+    df_keys = ['chip', 'pair', 'length', 'd_length', 'act_energy', 'd_act_energy', 'alpha0', 'd_alpha0',
+               'alpha1', 'd_alpha1', 'alpha2', 'ns', 'd_ns', 'i0', 'd_i0']
     if os.path.exists(csv_path):
         df = pd.read_csv(csv_path)
+        for k in df_keys:
+            if k not in df:
+                df[k] = [None for i in df.index]
+        df = df[df_keys]
     else:
-        df = pd.DataFrame({
-            'chip': [],
-            'pair': [],
-            'length': [],
-            'd_length': [],
-            'alpha0': [],
-            'd_alpha0': [],
-            'alpha1': [],
-            'd_alpha1': [],
-            'alpha2': [],
-            'ns': [],
-            'd_ns': [],
-            'i0': [],
-            'd_i0': [],
-        })
+        df = pd.DataFrame({k: [] for k in df_keys})
 
     for chip in data_dict:
         dh.load_chip(chip)
@@ -85,29 +79,23 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
         for pair in data_dict[chip]:
             nums = data_dict[chip][pair]['nums']
             names = np.array([f"{chip}_{i}" for i in nums])
-            names = names[np.argsort(ur.Quantity.from_list([dh.props[name]['temperature'] for name in names]))]
+            names = names[np.argsort(a.qlist_to_qarray([dh.props[name]['temperature'] for name in names]))]
 
-            dh.load(names[0])
-            print("Chip {}, Pair {} of length {}.".format(chip, pair, fmt(dh.get_length())))
-            fields = fields[(fields + delta_field) * dh.get_length() < dh.prop['bias']]
+            dh.load(names)
+            length = dh.get_length()
+            length_, dlength, _ = a.separate_measurement(length)
+            print(f"Chip {chip}, Pair {pair} of length {fmt(length)}.")
+            fields = fields[(fields + delta_field) * length < dh.props[names[0]]['bias']]
 
-            if 'noise_level' in data_dict[chip][pair]:
-                nl = data_dict[chip][pair]['noise_level']
-            else:
-                nl = noise_level
+            if plot_iv:
+                plot_iv_with_inset(dh, names, res_dir)
 
-            for i, name in enumerate(names):
-                print(f"\rLoading {i+1} of {len(names)}.", end='', flush=True)
-                dh.load(name)
-                if plot_iv:
-                    plot_iv_with_inset(dh, res_dir)
-            print()
+            df_index = np.nonzero(np.array((df['chip'] == chip) * (df['pair'] == pair)))[0]
 
-            temperature, conductance = get_temperature_dependence(dh, names, fields, delta_field, noise_level=nl)
+            temperature, conductance = get_temperature_dependence(dh, names, fields, delta_field)
 
             # plot temperature dependence
-            temp_win = [101, 350] * ur.K
-            mask = a.is_between(temperature, temp_win)
+            indices = a.is_between(temperature, [101, 350]*ur.K)
             x = 100 / temperature
             x_, dx, ux = a.separate_measurement(x)
             fig, ax = plt.subplots()
@@ -117,12 +105,12 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
                 y = conductance[i]
                 y_, dy, uy = a.separate_measurement(y)
                 ax.errorbar(x_, y_, xerr=dx, yerr=dy, fmt='--o', zorder=i, c=cols[i])
-                if i == 0:
-                    coeffs, model = a.fit_exponential(x[mask], y[mask])
-                    if coeffs is not None:
-                        act_energy = (- coeffs[0] * 100 * ur.k_B).to('meV')
-                        print("Activation energy:", fmt(act_energy))
-                        ax.plot(x_[mask], model(x_[mask]), c='r', zorder=len(fields), label=fr"fit ($U_A = {fmt(act_energy, latex=True)}$)")
+                if i == 0 and np.sum(indices) > 1:
+                    coeffs, model = a.fit_exponential(x[indices], y[indices])
+                    act_energy = (- coeffs[0] * 100 * ur.k_B).to('meV')
+                    act_energy_, dact_energy, _ = a.separate_measurement(act_energy)
+                    print("Activation energy:", fmt(act_energy))
+                    ax.plot(x_[indices], model(x_[indices]), c='r', zorder=len(fields), label=fr"fit ($U_A = {fmt(act_energy, latex=True)}$)")
             ax.legend()
             ax.set_xlabel(r"$\frac{{100}}{{T}}$" + ulbl(ux))
             ax.set_ylabel(r"$G$" + ulbl(uy))
@@ -132,18 +120,21 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
             fig.savefig(res_image)
             plt.close()
 
-            # Power law
+            #############
+            # Power law #
+            #############
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 9))
             fig.suptitle(f'Power Law ({chip} {pair})')
-            # temperature dependence at low bias
-            temp_win = [0 * ur.K, 100 * ur.K]
-            mask = a.is_between(temperature, temp_win)
-            x = temperature[mask]
-            y = conductance[0][mask]
+
+            # power law temperature dependence at low bias
+            indices = a.is_between(temperature, [20, 100]*ur.K)
+            x = temperature[indices]
+            y = conductance[0][indices]
             x, y = a.strip_nan(x, y)
-            coeffs, model = a.fit_powerlaw(x, y, check_nan=False)
-            if coeffs is not None:
+            if np.sum(indices) > 1:
+                coeffs, model = a.fit_powerlaw(x, y, check_nan=False)
                 alpha0 = coeffs[0]
+                alpha0_, dalpha0, _ = a.separate_measurement(alpha0)
                 print("alpha0:", fmt(alpha0))
                 x_, dx, ux = a.separate_measurement(x)
                 y_, dy, uy = a.separate_measurement(y)
@@ -154,17 +145,20 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
             ax1.set_xlabel(r"$T$" + ulbl(ux))
             ax1.set_ylabel(r"$G$" + ulbl(uy))
             ax1.legend()
-            # bias dependence at low temperature
+
+            # power law bias dependence at low temperature
             if 'lowtemp_highbias_num' in data_dict[chip][pair]:
                 lowtemp_highbias_name = f"{chip}_{data_dict[chip][pair]['lowtemp_highbias_num']}"
             else:
                 lowtemp_highbias_name = names[0]
             dh.load(lowtemp_highbias_name)
-            x = dh.raw['bias']
-            y = dh.get_conductance(correct_offset=True, time_win=[0.25, 0.75], field_win=[0.2, np.inf]*ur['V/um'])
+            mask = dh.get_mask(field_win=[0.3*ur['V/um'], 1])
+            x = dh.get_bias()
+            y = dh.get_conductance(mask=mask)
             x, y = a.strip_nan(x, y)
             coeffs, model = a.fit_powerlaw(x, y, check_nan=False)
             alpha1 = coeffs[0]
+            alpha1_, dalpha1, _ = a.separate_measurement(alpha1)
             print("alpha1:", fmt(alpha1))
             x_, dx, ux = a.separate_measurement(x)
             y_, dy, uy = a.separate_measurement(y)
@@ -180,15 +174,14 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
             fig.savefig(res_image)
             plt.close()
 
-            # universal scaling curve
+            ###########################
+            # universal scaling curve #
+            ###########################
 
             # plot scaled data
-            alpha0_, dalpha0, _ = a.separate_measurement(alpha0)
-            alpha1_, dalpha1, _ = a.separate_measurement(alpha1)
+            indices = np.nonzero(temperature < 110*ur.K)
             alpha2 = alpha0_
             fig, ax = plt.subplots()
-            fig.suptitle('Universal Scaling Curve')
-            indices = np.nonzero(temperature < 110*ur.K)
             cbar, cols = get_cbar_and_cols(fig, a.strip_err(temperature[indices]), log=True, ticks=[10, 15, 30, 60, 100])
             cbar.ax.set_ylabel(r"$T$" + ulbl(ur.K))
             x, y = [], []
@@ -197,11 +190,14 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
             lines = []
             for i, name in enumerate(names[indices]):
                 dh.load(name)
-                mask = dh.get_mask(current_win=[3*ur.pA, 1], time_win=[0.25, 0.5], field_win=[0*ur['V/um'], 1])
-                temperature.append(dh.raw['temperature'][mask].m)
-                current.append(dh.raw['current'][mask].m)
-                x.append((ur.e * dh.raw['bias'][mask] / (2 * np.pi * ur.k_B * dh.raw['temperature'][mask])).to(''))
-                y.append(dh.raw['current'][mask] / (dh.raw['temperature'][mask] ** (1 + alpha2)))
+                mask = dh.get_mask(current_win=[4*ur.pA, 1])
+                temperature_i = dh.get_temperature()[mask]
+                bias_i = dh.get_bias()[mask]
+                current_i = dh.get_current()[mask]
+                temperature.append(temperature_i.m)
+                current.append(current_i.m)
+                x.append((ur.e * bias_i / (2 * np.pi * ur.k_B * temperature_i)).to(''))
+                y.append(current_i / (temperature_i ** (1 + alpha2)))
                 lines.append(ax.plot(x[i].m, y[i].m, '.', zorder=i, c=cols[i])[0])
             x = np.concatenate(x)
             y = np.concatenate(y)
@@ -211,15 +207,11 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
             ax.set_ylabel(r'$\frac{I}{T^{1+\alpha}}$ ' + ulbl(uy))
             ax.set_xscale('log')
             ax.set_yscale('log')
+            ax.set_ylim([np.amin(y_) / 3, 3 * np.amax(y_)])
 
             if adjust_alpha:
                 res_image = os.path.join(res_dir, f"{chip}_{pair}_universal_scaling_adjusted.png")
-
-                # Make a horizontal slider to adjust parameter alpha
-                sl_ax = plt.axes([0.2, 0.8, 0.4, 0.03])
-                sl = Slider(sl_ax, r'$\alpha$', valmin=1, valmax=10, valinit=alpha2)
-                bt_ax = plt.axes([0.2, 0.6, 0.15, 0.07])
-                bt = Button(bt_ax, r'Confirm $\alpha$')
+                fig.suptitle(r'Universal Scaling Curve (Adjusted $\alpha$)')
 
                 def update(val):
                     y_ = []
@@ -232,14 +224,23 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
                     fig.canvas.draw_idle()
                     return y_
 
-                ss = SliderSetter(sl, bt, update)
-                alpha2 = ss.run()
-                print("alpha2:", fmt(alpha2))
+                if df.loc[df_index, 'alpha2'].empty:
+                    # Make a horizontal slider to adjust parameter alpha
+                    sl_ax = plt.axes([0.2, 0.8, 0.4, 0.03])
+                    sl = Slider(sl_ax, r'$\alpha$', valmin=1, valmax=10, valinit=alpha2)
+                    bt_ax = plt.axes([0.2, 0.6, 0.15, 0.07])
+                    bt = Button(bt_ax, r'Confirm $\alpha$')
+
+                    ss = SliderSetter(sl, bt, update)
+                    alpha2 = ss.run()
+                else:
+                    alpha2 = a.q_from_df(df.loc[df_index], 'alpha2').m
                 y_ = update(alpha2)
+                print("alpha2:", fmt(alpha2))
             else:
                 res_image = os.path.join(res_dir, f"{chip}_{pair}_universal_scaling.png")
+                fig.suptitle('Universal Scaling Curve')
 
-            # fit data
             def f(beta, x):
                 ns, i0 = beta
                 return i0 * np.sinh(np.pi * x / ns) * np.abs(gamma((2 + alpha2)/2 + 1j*x/ns))**2
@@ -251,29 +252,24 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
                 log='', already_separated=True, debug=False,
             )
             ns, i0 = coeffs
+            ns_, dns, _ = a.separate_measurement(ns)
+            i0_, di0, _ = a.separate_measurement(i0)
             print(f'ns: {fmt(ns)}\ni0: {fmt(i0)}')
 
             x_ = np.linspace(np.amin(x_), np.amax(x_), int(1e3))
-            label = fr'fit ($\alpha = {fmt(alpha0, latex=True)}$, $N_{{sites}} = {fmt(ns, latex=True)}$)'
+            label = fr'fit ($\alpha = {fmt(alpha2, latex=True)}$, $N_{{sites}} = {fmt(ns, latex=True)}$)'
             ax.plot(x_, model(x_), zorder=100, c='r', label=label)
             ax.legend()
-            if adjust_alpha:
-                plt.show()
-                res_image = os.path.join(res_dir, f"{chip}_{pair}_universal_scaling_adjusted.png")
-                csv_path = os.path.join(res_dir, 'params_adjusted.csv')
-            else:
-                res_image = os.path.join(res_dir, f"{chip}_{pair}_universal_scaling.png")
             fig.savefig(res_image)
             plt.close()
 
-            length_, dlength, _ = a.separate_measurement(dh.get_length())
-            ns_, dns, _ = a.separate_measurement(ns)
-            i0_, di0, _ = a.separate_measurement(i0)
             df0 = pd.DataFrame({
                 'chip': [chip],
                 'pair': [pair],
                 'length': [length_],
                 'd_length': [dlength],
+                'act_energy': [act_energy_],
+                'd_act_energy': [dact_energy],
                 'alpha0': [alpha0_],
                 'd_alpha0': [dalpha0],
                 'alpha1': [alpha1_],
@@ -284,14 +280,12 @@ def main(data_dict, noise_level=1*ur.pA, adjust_alpha=False, show=False, plot_iv
                 'i0': [i0_],
                 'd_i0': [di0],
             })
-            index = np.nonzero(np.array((df['chip'] == chip) * (df['pair'] == pair)))[0]
-            if len(index) > 0:
-                df.iloc[index] = df0.iloc[0]
-            else:
+            if df.loc[df_index].empty:
                 df = df0.merge(df, how='outer')
-
+            else:
+                df.iloc[df_index] = df0.iloc[0]
+            dh.clear()
             print()
-
     df.to_csv(csv_path, index=False)
 
 
@@ -326,7 +320,6 @@ if __name__ == "__main__":
         'SPC2': {
             "P2-P4": {
                 'nums': np.arange(45, 74),
-                'lowtemp_lowbias_num': 45,
                 'lowtemp_highbias_num': 74,
             },
             "P17-P18": {
@@ -334,17 +327,14 @@ if __name__ == "__main__":
                     np.arange(78, 102),
                     np.arange(103, 109)
                 ]),
-                'lowtemp_lowbias_num': 78,
                 'lowtemp_highbias_num': 279,
             },
             "P15-P17": {
                 'nums': np.arange(124, 155),
-                'lowtemp_lowbias_num': 124,
                 'lowtemp_highbias_num': 280,
             },
             "P7-P8": {
                 'nums': np.arange(156, 186),
-                'lowtemp_lowbias_num': 156,
                 'lowtemp_highbias_num': 278,
             },
             "P13-P14": {
@@ -354,12 +344,10 @@ if __name__ == "__main__":
                     np.arange(204, 208),
                     np.arange(209, 226)
                 ]),
-                'lowtemp_lowbias_num': 195,
                 'lowtemp_highbias_num': 277,
             },
             "P16-P17": {
                 'nums': np.arange(227, 258),
-                'lowtemp_lowbias_num': 227,
                 'lowtemp_highbias_num': 281,
             },
             "P15-P16": {
@@ -367,17 +355,14 @@ if __name__ == "__main__":
                     np.arange(283, 301),
                     [301, 303, 305, 307, 309, 311, 313, 316, 318, 320, 328, 330, 332],
                 ]),
-                'lowtemp_lowbias_num': 283,
                 'lowtemp_highbias_num': 282,
             },
             "P1-P4": {
                 'nums': np.arange(335, 366),
-                'lowtemp_lowbias_num': 335,
                 'lowtemp_highbias_num': 334,
             },
             "P1-P15": {
                 'nums': np.arange(367, 396),
-                'lowtemp_lowbias_num': 367,
                 'lowtemp_highbias_num': 366,
             },
         },
@@ -394,7 +379,6 @@ if __name__ == "__main__":
                     np.arange(172, 174),
                     [192],
                 ]),
-                'lowtemp_lowbias_num': 24,
                 'lowtemp_highbias_num': 5,
             },
         },
@@ -428,21 +412,22 @@ if __name__ == "__main__":
     ax.legend()
     fig.savefig(res_image)
     plt.close()
-    df.to_csv(csv_path, index=False)
 
     df_latex = df.loc[:, ['chip', 'pair']]
     keys = ['length', 'alpha0', 'ns']
     titles = ['Length' + ulbl(ur.um), r'$\alpha$', r'$N_{sites}$']
-    if adjust_alpha:
-        tex_path = os.path.join(res_dir, 'params_adjusted.tex')
-        keys.append('alpha2')
-        titles.append(r'$\alpha_{manual}')
-    else:
-        tex_path = os.path.join(res_dir, 'params.tex')
     for key, title in zip(keys, titles):
         ux = ur.um if key == 'length' else ur['']
         x_ = np.array(df[key])
         dx = np.array(df['d_' + key])
         x = [fmt((l_ * ur['']).plus_minus(dl).m, latex=True) for l_, dl in zip(x_, dx)]
         df_latex[title] = x
+    if adjust_alpha:
+        x_ = np.array(df['alpha2'])
+        x = [fmt((l_), latex=True) for l_ in x_]
+        title = r'$\alpha_{manual}'
+        df_latex[title] = x
+        tex_path = os.path.join(res_dir, 'params_adjusted.tex')
+    else:
+        tex_path = os.path.join(res_dir, 'params.tex')
     df_latex.set_index(['chip', 'pair']).style.to_latex(tex_path)