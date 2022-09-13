cf0b3255722c08562281002a2e510c41e06e5aa2
MatteoDep <matteo.dep97@gmail.com>
2022-09-13 19:51:58 +0200

    better plots

--- data_plotter.py
+++ data_plotter.py
@@ -10,9 +10,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import Normalize
 from matplotlib import ticker
-import warnings
 import analysis as a
-from analysis import ur
+from analysis import ur, fmt
 
 
 RES_DIR = os.path.join('results', 'data')
@@ -59,7 +58,50 @@ def plot_fit(dh, data_dict):
             plt.close()
 
 
-def plot_iv(dh, names, zoom='auto', appendix=''):
+def plot_ivs(dh, data_dict):
+    for chip in data_dict:
+        dh.load_chip(chip)
+        nums = data_dict[chip]['nums']
+        names = np.array([f"{chip}_{i}" for i in nums])
+        names_dict = {}
+        lengths = []
+        for name in names:
+            dh.load(name)
+            length = dh.get_length()
+            key = fmt(length)
+            if key not in names_dict:
+                names_dict[key] = []
+                lengths.append(length.m)
+            names_dict[key].append(name)
+        idx = np.argsort(lengths)
+        n_sf = len(names_dict)
+        fig = plt.figure(constrained_layout=True, figsize=(18, n_sf*9))
+        fig.suptitle(f'Example I-Vs from chip {chip}')
+        subfigs = fig.subfigures(nrows=n_sf, ncols=1)
+        if n_sf == 1:
+            subfigs = [subfigs]
+            pairs = []
+        for i, length in enumerate(names_dict):
+            subfigs[idx[i]].suptitle(f'Segment of length {length}')
+            axs = subfigs[idx[i]].subplots(1, 2)
+            for name in names_dict[length]:
+                dh.load(name)
+                inj = dh.prop['injection'].lower()
+                if inj == '2p':
+                    dh.plot(axs[0], mode='i/v')
+                    axs[0].set_title('2-probe')
+                else:
+                    dh.plot(axs[1], mode='v/i')
+                    axs[1].set_title('4-probe')
+            pairs.append(dh.prop['pair'])
+        res_image = os.path.join(
+            RES_DIR, f"{dh.chip}_{'_'.join(pairs)}_iv_2-4p.png"
+        )
+        fig.savefig(res_image)
+        plt.close()
+
+
+def plot_iv(dh, data_dict, highbias=False):
     for chip in data_dict:
         dh.load_chip(chip)
         nums = data_dict[chip]['nums']
@@ -67,25 +109,19 @@ def plot_iv(dh, names, zoom='auto', appendix=''):
         for name in names:
             dh.load(name)
             fig, ax = plt.subplots(figsize=(12, 9))
-            dh.plot(ax)
-            if zoom == 'auto':
-                zoom_ = dh.prop['temperature'] < 55*ur.K
-            else:
-                zoom_ = zoom
-            if zoom_:
+            fig.suptitle(f"Example I-V at {dh.prop['temperature']}")
+            dh.plot(ax,)
+            if not highbias and dh.prop['temperature'] < 55*ur.K:
                 mask = dh.get_mask([-0.05, 0.05] * ur['V/um'])
                 ax_in = ax.inset_axes([0.65, 0.08, 0.3, 0.3])
                 dh.plot(ax_in, mask=mask, set_xy_label=False)
                 ax_in.set_xmargin(0)
                 ax_in.set_ymargin(0)
                 ax.indicate_inset_zoom(ax_in)
-            fig.suptitle(f"{dh.chip} {dh.prop['pair']} at {dh.prop['temperature']}")
             res_image = os.path.join(
-                RES_DIR, f"{dh.chip}_{dh.prop['pair']}_iv_{a.fmt(dh.prop['temperature'], sep='')}{appendix}.png"
+                RES_DIR, f"{dh.chip}_{dh.prop['pair']}_iv_{fmt(dh.prop['temperature'], sep='')}{'_highbias' if highbias else ''}.png"
             )
-            with warnings.catch_warnings():
-                warnings.simplefilter('ignore', category=UserWarning)
-                fig.savefig(res_image, dpi=100)
+            fig.savefig(res_image)
             plt.close()
 
 
@@ -103,7 +139,7 @@ def plot_gate_trace(dh, title=None, res_names=None):
                 axs = [axs]
             for j, m in enumerate(modes):
                 axs[j] = dh.plot(axs[j], mode=m)
-            fig.suptitle(f"Gate Trace ({chip} {dh.prop['pair']})")
+            fig.suptitle(f"Example Gate Trace on chip {chip}")
             fig.tight_layout()
             res_image = os.path.join(RES_DIR, f"{chip}_{dh.prop['pair']}_gate_trace.png")
             fig.savefig(res_image)
@@ -126,7 +162,7 @@ def get_cbar_and_cols(fig, values, log=False, ticks=None, **kwargs):
         cbar.ax.yaxis.set_major_locator(ticker.FixedLocator((np.log(ticks))))
     else:
         cbar.ax.yaxis.set_major_locator(ticker.FixedLocator((ticks)))
-    cbar.ax.yaxis.set_major_formatter(ticker.FixedFormatter(([a.fmt(f) for f in ticks])))
+    cbar.ax.yaxis.set_major_formatter(ticker.FixedFormatter(([fmt(f) for f in ticks])))
     return cbar, cols
 
 
@@ -148,17 +184,30 @@ if __name__ == "__main__":
             'nums': [30],
         },
         'SPC2': {
-            'nums': range(484, 498),
+            'nums': [492],
         },
     }
     plot_gate_trace(dh, data_dict)
 
     data_dict = {
         'SPC2': {
+            'nums': [5, 19],
+        },
+        'SPC3': {
+            'nums': [7, 15],
+        },
+        'SQC1': {
+            'nums': [9, 10, 14, 23, 24, 28],
+        },
+    }
+    plot_ivs(dh, data_dict)
+
+    data_dict = {
+        'SPC2': {
             'nums': [282],
         },
     }
-    plot_iv(dh, data_dict, zoom=False, appendix='_highbias')
+    plot_iv(dh, data_dict, highbias=True)
     data_dict = {
         'SPC2': {
             'nums': [283, 297, 305, 332],
