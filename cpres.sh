52d919584835aea6c4c9c7e0a1b5c4a36f348e91
MatteoDep <matteo.dep97@gmail.com>
2022-10-15 02:56:26 +0200

    better handling of duplicates and paths

--- cpres.sh
+++ cpres.sh
@@ -6,13 +6,29 @@ report_images_dir='../report/images'
 subdirs='data light length temperature gate'
 
 for subdir in $subdirs; do
-	sourcedir="./results/$subdir"
-	cp -r "$sourcedir" "$report_images_dir/"
-	for file in "$report_images_dir/$subdir"/*.png; do
-		if [ -f "$file" ]; then
-			filenoext=${file%.*}
-			convert "$file" -density 100 "$filenoext".pdf
-			rm "$file"
+	sourcedir="results/$subdir"
+	destdir="$report_images_dir/$subdir"
+	for source_file in "$sourcedir"/*; do
+		if [ -f "$source_file" ]; then
+			dest_file="$destdir/${source_file##*/}"
+			dest_base="${dest_file%.*}"
+			ext="${source_file##*.}"
+			case "$ext" in
+				png)
+					printf "converting %s to %s.pdf\n" "$source_file" "$dest_base"
+					convert "$source_file" -density 100 "$dest_base".pdf
+					;;
+				tex)
+					printf "copying %s to %s\n" "$source_file" "$dest_file"
+					cp "$source_file" "$dest_file"
+					;;
+				csv)
+					printf "skipping %s\n" "$source_file"
+					;;
+				*)
+					printf "unrecognized extension of file %s, skipping...\n" "$source_file"
+					;;
+			esac
 		fi
 	done
 done
